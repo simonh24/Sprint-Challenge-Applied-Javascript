707ea6ff09e402da9b331812758d69dd78aa5e71
Simon <simonh72498@gmail.com>
2020-05-08 13:38:01 -0400

    finished mvp

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -18,3 +18,38 @@
 // </div>
 //
 // Use your function to create a card for each of the articles and add the card to the DOM.
+
+function cardMaker(cardAttrs) {
+    const {headline, authorPhoto, authorName} = cardAttrs;
+    const divCard = document.createElement("div");
+    const divHeadline = document.createElement("div");
+    const divAuthor = document.createElement("div");
+    const divContainer = document.createElement("div");
+    const imgAuthor = document.createElement("img");
+    const spanAuthor = document.createElement("span");
+    divCard.classList.add("card");
+    divHeadline.classList.add("headline");
+    divAuthor.classList.add("author");
+    divContainer.classList.add("img-container");
+    divHeadline.textContent = headline;
+    imgAuthor.setAttribute("href", authorPhoto);
+    spanAuthor.textContent = authorName;
+    divCard.appendChild(divHeadline);
+    divCard.appendChild(divAuthor);
+    divAuthor.appendChild(divContainer);
+    divContainer.appendChild(imgAuthor);
+    divAuthor.appendChild(spanAuthor);
+    return divCard;
+}
+
+axios.get("https://lambda-times-backend.herokuapp.com/articles")
+    .then(response => {
+        const allArticles = response.data.articles;
+        const allArticlesArr = Object.keys(allArticles);
+        const entry = document.querySelector("div.cards-container");
+        for (let i = 0; i < allArticlesArr.length; i++) {
+            for (let j = 0; j < allArticles[allArticlesArr[i]].length; j++) {
+                entry.appendChild(cardMaker(allArticles[allArticlesArr[i]][j]));
+            }
+        }
+    })
